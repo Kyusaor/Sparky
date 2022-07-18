9a86a77539f3e354b587af79d63291c63fe4dcc1
Kyusakick <banque.cdl@gmail.com>
2022-07-18 16:02:44 +0200

    ajout champ description

--- modules/commands/lien.js
+++ modules/commands/lien.js
@@ -1,21 +1,22 @@
 const config = require('../../data/config.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const utils = require('../utils.js');
 
 module.exports = {
 
     name: 'lien',
+    description: 'Envoie le lien pour m\'ajouter sur votre serveur',
 
-    description:'Envoie le lien pour m\'ajouter sur votre serveur',
+    isDev: true,
 
-    level: 1,
+    data: 
+        new SlashCommandBuilder()
+            .setName('lien')
+            .setDescription('Envoie le lien pour m\'ajouter sur votre serveur'),
 
-    run(args){
+    run(intera, bot){
 
-        //Récupération des variables
-        let msg = args.msg;
-        let bot = args.bot;
-
-        msg.channel.send("Voici le lien pour m'ajouter sur votre serveur:\n<https://discord.com/oauth2/authorize?client_id=" + bot.user.id + "&scope=bot&permissions=" + config.botperm + ">")
-        .catch(error => console.log(utils.displayConsoleHour() + "Impossible d'envoyer le lien dans le salon " + msg.channel.id + " (Serveur " + msg.guild.name + ")"))
+        intera.reply("Voici le lien pour m'ajouter sur votre serveur:\n<https://discord.com/oauth2/authorize?client_id=" + bot.user.id + "&scope=bot&permissions=" + config.botperm + ">")
+        .catch(error => console.log(utils.displayConsoleHour() + "Impossible d'envoyer le lien dans le salon " + intera.channel.id + " (Serveur " + intera.guild.name + ")"))
     }
 }
