166721eeb002acd15662f8a5f18fd88eb06b9196
Kyusakick <banque.cdl@gmail.com>
2022-07-18 16:02:25 +0200

    ajout champ description

--- modules/commands/contact.js
+++ modules/commands/contact.js
@@ -4,6 +4,7 @@ const utils = require('../utils.js');
 module.exports = {
 
     name: 'contact',
+    description: 'Nous contacter (discord ou email)',
 
     data:
         new SlashCommandBuilder()
