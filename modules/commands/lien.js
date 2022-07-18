74773a2b38d939d78ef200dcf68e391b5ddae4e5
Kyusakick <banque.cdl@gmail.com>
2022-07-18 17:36:55 +0200

    ajout log err envoi reponse

--- modules/commands/lien.js
+++ modules/commands/lien.js
@@ -17,6 +17,6 @@ module.exports = {
     run(args){
 
         args.intera.reply("Voici le lien pour m'ajouter sur votre serveur:\n<https://discord.com/oauth2/authorize?client_id=" + args.bot.user.id + "&scope=bot&permissions=" + config.botperm + ">")
-        .catch(error => console.log(utils.displayConsoleHour() + "Impossible d'envoyer le lien dans le salon " + args.intera.channel.id + " (Serveur " + args.intera.guild.name + ")"))
+        .catch(error => utils.errorSendReply('lien', args))
     }
 }
