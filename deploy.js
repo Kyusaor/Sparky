f8675df5657487faa28685b1c8f14dd00e70e4a5
Kyusakick <banque.cdl@gmail.com>
2022-08-03 18:16:56 +0200

    fix deploy double commande dev

--- deploy.js
+++ deploy.js
@@ -1,39 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord-api-types/v10');
 const { kyu } = require('./data/config.js');
 const { token } = require('./data/private.js');
 const fs = require('fs');
-
-
-
-//listage des commmandes
-
-let commands = []
-let devCommands = []
-const rest = new REST({ version: '9' }).setToken(token);
-
-const commandFiles = fs.readdirSync('./modules/commands').filter(file => file.endsWith('.js'));
-
-for(file of commandFiles) {
-    const commandData = require(`./modules/commands/${file}`);
-    if(commandData.isDev) devCommands.push(commandData.data)
-    else commands.push(commandData.data)
-}
-
-commands.map(command => command.toJSON());
-devCommands.map(command => command.toJSON());
-
-rest.put(Routes.applicationCommands('746783550866456716'), { body: commands })
-    .then(() => console.log("Commandes déployées avec succès"))
-    .catch(console.error)
-
-rest.put(Routes.applicationGuildCommands('746783550866456716', '632957557375500299'), { body: devCommands })
-    .then(() => console.log("Commandes dev déployées avec succès"))
-    .catch(console.error)
+const rest = new REST({ version: '10' }).setToken(token);
 
 /*
-//A run pour effacer les commandes
+//Suppression des / commandes globales
 rest.get(Routes.applicationCommands('746783550866456716'))
     .then(data => {
         const promises = [];
@@ -45,6 +19,7 @@ rest.get(Routes.applicationCommands('746783550866456716'))
     }
 );
 
+//Suppression des / commandes dev
 rest.get(Routes.applicationGuildCommands('746783550866456716', '632957557375500299'))
     .then(data => {
         const promises = [];
@@ -54,4 +29,29 @@ rest.get(Routes.applicationGuildCommands('746783550866456716', '6329575573755002
         }
         return Promise.all(promises);
     }
-);*/
+);
+*/
+
+//listage des commmandes
+let commands = []
+let devCommands = []
+
+const commandFiles = fs.readdirSync('./modules/commands').filter(file => file.endsWith('.js'));
+
+for(file of commandFiles) {
+    const commandData = require(`./modules/commands/${file}`);
+    if(commandData.isDev) devCommands.push(commandData.data)
+    else commands.push(commandData.data)
+}
+
+commands.map(commands => commands.toJSON());
+devCommands.map(command => command.toJSON());
+
+//Déploiement des / commandes
+rest.put(Routes.applicationCommands('746783550866456716'), { body: commands })
+    .then(() => console.log("Commandes déployées avec succès"))
+    .catch(console.error)
+
+rest.put(Routes.applicationGuildCommands('746783550866456716', '632957557375500299'), { body: devCommands })
+    .then(() => console.log("Commandes dev déployées avec succès"))
+    .catch(console.error)
