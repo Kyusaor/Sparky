dee2e5ce115bb1fc11cac420ef95577ac3a066ea
Kyusakick <banque.cdl@gmail.com>
2022-08-03 19:37:53 +0200

    ajout

--- modules/commands/credits.js
+++ modules/commands/credits.js
@@ -1,29 +1,35 @@
 const Discord = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const utils = require('../utils.js');
 
 module.exports = {
 
     name:"credits",
-
     description:"Remerciements à ceux qui ont contribué au développement du bot",
 
-    level: 1,
+    data:
+        new SlashCommandBuilder()
+            .setName("credits")
+            .setDescription("Remerciements à ceux qui ont contribué au développement du bot")
+            .setDMPermission(false),
 
     run:function(args){
 
-        //Récupération des arguments
-        let msg = args.msg;
-        let kyu = args.kyu;
 
         //Création du message de remerciement
-        let embed = new Discord.MessageEmbed()
-        .setTitle("Remerciements")
-        .setDescription("La liste des personnes qui ont aidé de près ou de loin au développement du bot")
-        .setColor([246,19,19])
-        .setFooter("Développé par " + kyu.tag, kyu.displayAvatarURL())
-        .addField("Les testeurs", "Pour m'avoir aidé à la conception du bot et l'amélioration des rendus")
-        .addField("Les bêta testeurs", "Qui ont accepté de servir de cobayes pour un bot presque fini")
-        .addField("Heph, développeur de HephBot", "Pour avoir aimablement accepté l'utilisation de ses compos monstres")
-        .addField("Et enfin...", "Tous les utilisateurs du bot pour avoir accordé leur confiance à ce projet :heart:")
-        msg.channel.send(embed)
+        let embed = new EmbedBuilder()
+            .setTitle("Remerciements")
+            .setDescription("La liste des personnes qui ont aidé de près ou de loin au développement du bot")
+            .setColor([246,19,19])
+            .setFooter({text: "Développé par " + args.kyu.tag, iconURL: args.kyu.displayAvatarURL()})
+            .addFields([
+                {name: "Les testeurs", value: "Pour m'avoir aidé à la conception du bot et l'amélioration des rendus"},
+                {name: "Les bêta testeurs", value: "Qui ont accepté de servir de cobayes pour un bot presque fini"},
+                {name: "Les donateurs", value: "Pour m'avoir aidé à soutenir le coût du bot"},
+                {name: "Et enfin...", value: "Tous les utilisateurs du bot pour avoir accordé leur confiance à ce projet :heart:"},
+            ])
+
+        args.intera.reply({embeds: [embed]})
+        //.catch(err => utils.errorSendReply('credits', args))
     }
 }
