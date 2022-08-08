9e853119f0a1f3588548f95cb089056b73643821
Kyusakick <banque.cdl@gmail.com>
2022-08-08 18:36:58 +0200

    ajout reparamétrage

--- modules/commands/veilleur.js
+++ modules/commands/veilleur.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
 
 module.exports = {
 
@@ -25,6 +25,45 @@ module.exports = {
         //Confirmation puis réinitialisation des rôles
         let gpconfig = args.gpconfig[args.intera.guildId]
         
-        
+        if(gpconfig.ping) {
+
+            //Création du message
+            let boutons = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('reparam-oui')
+                        .setEmoji('✅')
+                        .setStyle(ButtonStyle.Secondary),
+                    
+                    new ButtonBuilder()
+                        .setCustomId('reparam-non')
+                        .setEmoji('❌')
+                        .setStyle(ButtonStyle.Secondary)
+                )
+            
+            args.intera.reply({ content: "Les notifications d'infernaux sont déjà définies, voulez-vous les reparamétrer?", components: [boutons]})
+
+            //Collecteur du bouton
+            let coll;
+            try {
+                const filter = (i) => i.user.id == args.intera.user.id && ['reparam-oui', 'reparam-non'].includes(i.customId)
+                coll = await args.intera.channel.awaitMessageComponent({ filter, time: 30000, componentType: ComponentType.Button}) 
+            }
+            catch { return args.intera.editReply({content: "Commande annulée", components: []})}
+            
+            if(coll.customId == 'reparam-non') return args.intera.editReply({content: "Commande annulée", components: []})
+
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.CDR);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.CDT);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.ID);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.IVR);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.IV);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.IDR);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.OJ);
+            await utils.deleteRole(bot, msg.guild.id, gpconfig.roles.OR);
+            await utils.deleteChan(bot, msg.guild.id, gpconfig.chan_notifs);
+            await utils.deleteChan(bot, msg.guild.id, gpconfig.chan_board);
+
+        }
     }
 }
