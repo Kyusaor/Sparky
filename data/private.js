35dcc84a14cf2a795692843e9330a4ba34c457c5
Kyusakick <banque.cdl@gmail.com>
2022-08-29 19:09:06 +0200

    token

--- data/private.js
+++ data/private.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //token: 'NjMyOTU2MTA5MTM2ODU1MDYw.XaM8UA.VKYDNyYN1h4kb-RGq1KE8A78Qyk',
+    token: 'NjMyOTU2MTA5MTM2ODU1MDYw.XaM8UA.VKYDNyYN1h4kb-RGq1KE8A78Qyk',
 
-    token: 'NzQ2NzgzNTUwODY2NDU2NzE2.X0FWYQ.fPgyEoaf8LspGtlr7Dl4aK0ez-I'
+    //token: 'NzQ2NzgzNTUwODY2NDU2NzE2.Gd1ocF.QVcpqeNn7eQV7wy1vdEXA8SgjcM8Zf63DFoZ-Y'
 }
