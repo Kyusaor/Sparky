794666240254a236315e4f6879914213d64a1fd4
Kyusakick <banque.cdl@gmail.com>
2022-08-29 19:47:55 +0200

    fix id bot

--- deploy.js
+++ deploy.js
@@ -8,11 +8,11 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 /*
 //Suppression des / commandes globales
-rest.get(Routes.applicationCommands('746783550866456716'))
+rest.get(Routes.applicationCommands('632956109136855060'))
     .then(data => {
         const promises = [];
         for (const command of data) {
-            const deleteUrl = `${Routes.applicationCommands('746783550866456716')}/${command.id}`;
+            const deleteUrl = `${Routes.applicationCommands('632956109136855060')}/${command.id}`;
             promises.push(rest.delete(deleteUrl));
         }
         return Promise.all(promises);
@@ -20,11 +20,11 @@ rest.get(Routes.applicationCommands('746783550866456716'))
 );
 
 //Suppression des / commandes dev
-rest.get(Routes.applicationGuildCommands('746783550866456716', '632957557375500299'))
+rest.get(Routes.applicationGuildCommands('632956109136855060', '632957557375500299'))
     .then(data => {
         const promises = [];
         for (const command of data) {
-            const deleteUrl = `${Routes.applicationGuildCommands('746783550866456716', '632957557375500299')}/${command.id}`;
+            const deleteUrl = `${Routes.applicationGuildCommands('632956109136855060', '632957557375500299')}/${command.id}`;
             promises.push(rest.delete(deleteUrl));
         }
         return Promise.all(promises);
@@ -48,10 +48,10 @@ commands.map(commands => commands.toJSON());
 devCommands.map(command => command.toJSON());
 
 //Déploiement des / commandes
-rest.put(Routes.applicationCommands('746783550866456716'), { body: commands })
+rest.put(Routes.applicationCommands('632956109136855060'), { body: commands })
     .then(() => console.log("Commandes déployées avec succès"))
     .catch(console.error)
 
-rest.put(Routes.applicationGuildCommands('746783550866456716', '632957557375500299'), { body: devCommands })
+rest.put(Routes.applicationGuildCommands('632956109136855060', '632957557375500299'), { body: devCommands })
     .then(() => console.log("Commandes dev déployées avec succès"))
     .catch(console.error)
