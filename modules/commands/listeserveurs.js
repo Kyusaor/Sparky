97ba883450e7588ec054405d23dca28046c0864c
Kyusakick <banque.cdl@gmail.com>
2022-09-05 18:03:07 +0200

    Ajout defile

--- modules/commands/listeserveurs.js
+++ modules/commands/listeserveurs.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const fs = require("fs");
-const { interaReply } = require("../utils");
+const { interaReply, errorSendReply } = require("../utils");
 
 module.exports = {
 
@@ -89,7 +89,51 @@ module.exports = {
         await interaReply({ embeds: [embed], components: [boutons]}, args.intera)
     },
 
-    defile(args){
+    async defile(intera, bot){
 
+        //Récupération de la liste de serveurs triée
+        let type, nextpage, filtre;
+        if(intera.customId.startsWith('members')) {type = "members"; filtre = "membres"}
+        else if(intera.customId.startsWith('name')) {type = "name"; filtre = "noms"}
+        else {type = "none"; filtre = "aucun"}
+
+        let listefile = JSON.parse(fs.readFileSync('./data/sortedguilds.json'));
+        let liste = listefile[type];
+
+        let page = Number(intera.message.embeds[0].footer.text[6])
+        if(intera.customId.endsWith('next')) nextpage = page + 1
+        else nextpage = page - 1
+        
+        //Création de l'embed
+        let embed = new EmbedBuilder()
+            .setTitle("__**Liste des serveurs du bot**__")
+            .setDescription("*filtre: " + filtre + "*")
+            .setColor([59, 229, 53])
+            .setFooter({ text: "Page [" + nextpage + "/" + Math.ceil(liste.length / 25) + "]"})
+
+        for(let i = 25 * (nextpage - 1); i < (25 * nextpage) && liste[i]; i++) {
+            let owner = await bot.users.fetch(liste[i].owner);
+            if(!owner?.id || !owner?.tag) owner = { tag: "Introuvable", id: "Indisponible" }
+            embed.addFields({ name: (i + 1) + ". **" + liste[i].name + "**", value: "__id__: " + liste[i].id + "\n__Owner__: " + owner.tag + " (" + owner.id + ")\n__Membres__: " + liste[i].membersCount })
+        }
+
+        //Création des boutons
+        let boutons = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId(type + '-previous')
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji('◀️'),
+
+                new ButtonBuilder()
+                    .setCustomId(type + '-next')
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji('▶️')
+            )
+
+        if(nextpage == 1) boutons.components[0].setDisabled(true)
+        else if (nextpage == Math.ceil(liste.length / 25)) boutons.components[1].setDisabled(true)
+
+        await intera.reply({ embeds: [embed], components: [boutons]}).catch(console.error)
     }
 }
