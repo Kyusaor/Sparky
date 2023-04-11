2891b4661c73f6d33835007679db62bdc33074c1
Kyusakick <banque.cdl@gmail.com>
2023-04-11 18:21:10 +0200

    add discord default values

---
+++ src/core/constants/values.ts
@@ -0,0 +1,18 @@
+export abstract class DiscordValues {
+
+    static readonly MAIN_GUILD = "632957557375500299";
+    static readonly DEV_DISCORD_ID = "370293090407153665";
+
+    static readonly channels = {
+        logs: {
+            CONNEXIONS: "656141877770321960",
+            SERVERS: "656141779116097536",
+            DM: "750464720204726333",
+            ERRORS: "1015306460672045259",
+            USERS: "656141824590872576",
+            DB: "784919405896400916",
+            HELL_EVENTS: "1013478125004398683",
+        },
+        HELL_EVENTS_BOARD: "647042988358238208"
+    };
+}
