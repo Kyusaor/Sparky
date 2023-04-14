a37dc52d4052b35d2ff97c7fae2880d4eb917246
Kyusakick <banque.cdl@gmail.com>
2023-04-14 17:29:05 +0200

    create consolelogger

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,8 +1,58 @@
+import consoleStamp from "console-stamp";
+import { createWriteStream, existsSync, mkdirSync } from "fs";
+
 export abstract class Utils {
 
-    static logErrors(err:any, crash:boolean) {
-        console.error(err);
+    static format2DigitsNumber(num: number) {
+        return num.toString().padStart(2, '0');
+    }
+
+}
+
+
+export class ConsoleLogger {
+    logger: Console;
+
+    constructor () {
+
+        let formatDate = {
+            day: Utils.format2DigitsNumber(new Date().getDate()),
+            month: Utils.format2DigitsNumber(new Date().getMonth()),
+            year: Utils.format2DigitsNumber(new Date().getFullYear())
+        };
+
+        //Check if directory exists and create it if not
+        let dir = `./logs/${formatDate.month}-${formatDate.year}`
+        if(!existsSync(dir)) {
+            mkdirSync(dir)
+        }
+
+        //Log into file
+        const output = createWriteStream( dir + `/stdout-${formatDate.day}-${formatDate.month}-${formatDate.year}.log`, { flags: 'a' }) as any;
+        const errorOutput = createWriteStream(dir + `/stderr-${formatDate.day}-${formatDate.month}-${formatDate.year}.log`, { flags: 'a' }) as any;
+        this.logger = new console.Console(output, errorOutput);
+
+        consoleStamp(this.logger, {
+            format: ":date(dd/mm/yyyy - HH:MM:ss)",
+            stderr: errorOutput,
+            stdout: output
+        });
+        consoleStamp(console, {
+            format: ":date(dd/mm/yyyy - HH:MM:ss)",
+        });
+
+        this.logger.info("Console paramétrée avec succès")
+    };
+
+    error(input:any, crash:boolean) {
+        console.error(input);
+        this.logger.error(input);
         if(crash)
             process.exit(1);
+    };
+
+    log(input:any) {
+        console.log(input);
+        this.logger.log(input);
     };
 }
