dd76711062ac409d4711822b34b707989f802c10
Kyusakick <banque.cdl@gmail.com>
2023-04-15 17:40:07 +0200

    add cron to schedule refresh console every day

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { Client } from 'discord.js';
 import { readFileSync } from 'fs';
 import { Config } from '../data/config.js';
 import { ConsoleLogger, Utils } from './core/utils.js';
+import cron from 'node-cron';
 
 let Console = new ConsoleLogger();
 const VERSION = JSON.parse(readFileSync('./package.json', 'utf-8')).version; // app version
 const bot = new Client(Config.clientParam);
+cron.schedule('0 0 * * *', () => {
+    Console = new ConsoleLogger();
+    Console.log("Refresh des logs effectué avec succès")
+})
 
 bot.login(Config.CURRENT_TOKEN)
 
