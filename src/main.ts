2bf1ecfd2fe56d915c2191c65cfde19f0364cabe
Kyusakick <banque.cdl@gmail.com>
2023-04-16 16:58:37 +0200

    add dev fetching at boot

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import {  Client } from 'discord.js';
+import {  Client, User } from 'discord.js';
 import { readFileSync } from 'fs';
 import { Config } from '../data/config.js';
 import { ConsoleLogger, Utils } from './core/utils.js';
 import cron from 'node-cron';
 import { fetchedChannelsAtBoot } from './core/constants/types.js';
+import { DiscordValues } from './core/constants/values.js';
 
 let Console = new ConsoleLogger();
 const VERSION = JSON.parse(readFileSync('./package.json', 'utf-8')).version; // app version
@@ -15,12 +16,16 @@ cron.schedule('0 0 * * *', () => {
 
 bot.login(Config.CURRENT_TOKEN)
 let chanList:fetchedChannelsAtBoot;
+let dev:User;
 
 //Executed when the bot starts
 bot.on('ready', async () => {
     try {
         Utils.statusLoop(bot);
         chanList = await Utils.fetchChannelsAtBoot();
+        dev = await bot.users.fetch(DiscordValues.DEV_DISCORD_ID);
+        if(!dev)
+            Console.error("Compte discord dev introuvable", true);
         Console.log(`\n\n             SPARKY\n\nBot discord Lords Mobile français\nDéveloppé par Kyusaki\n\nVersion: ${VERSION}\nClient: ${bot.user?.username}\nConsole:`)
     }
     catch (err) {
@@ -28,4 +33,4 @@ bot.on('ready', async () => {
     }
 })
 
-export { bot, Console, chanList };
+export { bot, Console, chanList, dev };
