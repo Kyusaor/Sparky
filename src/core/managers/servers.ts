24014808651d5b3d8e3b4f68238be7417130503f
Kyusakick <banque.cdl@gmail.com>
2023-05-01 20:33:42 +0200

    add editServerData fx server manager

--- src/core/managers/servers.ts
+++ src/core/managers/servers.ts
@@ -1,5 +1,5 @@
 import { Guild } from "discord.js";
-import { bot, db } from "../../main.js";
+import { Console, bot, db } from "../../main.js";
 import { PartialServer, Server } from "../constants/types.js";
 
 export class ServerManager {
@@ -11,9 +11,8 @@ export class ServerManager {
 
     static async buildBaseServerObject(serverId: string): Promise<Server> {
         let guild = await bot.guilds.cache.get(serverId);
-        if(!guild)
-            throw new Error(`Le serveur ${serverId} est introuvable`);
-        return { id: serverId, name:guild.name, active: true, language: "fr"}
+        let data:Server = { id: serverId, name: guild?.name || "Serveur introuvable", active: 1, language: "fr"};
+        return data
     }
 
     async checklistNewServers() {
@@ -21,13 +20,27 @@ export class ServerManager {
         if(!dbPresence) 
             await db.createServer(this.guild.id, this.guild.name);
         else if(await this.isActive()){
-            this.editServerData({active: true});
+            this.editServerData({active: 0});
         }
         //Finir ajouts (mp admin etc)
+        //Changer format donnée "active" en autre chose que 0
     }
 
     async editServerData(edits: PartialServer): Promise<void> {
+        let oldData = await db.fetchServerData(this.guild.id);
+        if(!oldData)
+            oldData = await ServerManager.buildBaseServerObject(this.guild.id);
+        let data:Server = {
+            id: edits.id || oldData.id,
+            name: edits.name || oldData.name,
+            active: edits.active || oldData.active,
+            language: edits.language || oldData.language
+        }
+        await db.editServerDatabase(data);
 
+        for(let dataName of Object.keys(edits)) {
+            Console.log(`${dataName}: ${oldData[dataName as keyof Server]} => ${edits[dataName as keyof Server]}`)
+        }
     }
 
     async isActive(): Promise<boolean> {
