e6e37df131596816ce8601917f2488eb87d0f6d9
Kyusakick <banque.cdl@gmail.com>
2023-05-03 16:17:39 +0200

    add translations

--- src/core/constants/translations.ts
+++ src/core/constants/translations.ts
@@ -4,28 +4,36 @@ import { textLanguage } from "./types";
 export class Translations {
 
     static getServerLanguage(language: textLanguage) {
-        let text = this.text.fr;
+        let text = this.displayText().fr
         return text
     }
 
-    static text = {
-        fr: {
-            global: {
-                tipsFooter: [
-                   {text: "Faites la commande /aide pour obtenir de l'aide !"},
-                   {text: `Développé avec amour par ${dev}`, iconURL: dev.displayAvatarURL()},
-                   {text: `Faites la commande /langage pour passer le bot en anglais !`},
-                   {text: `Faites /veilleur pour avoir les notifications d'évènements infernaux`},
-                   {text: `Fun fact: je suis le meilleur bot du monde`},
-                ]
-            }
-        },
-    
-        en: {
-            global: {
-                tipsFooter: [
-                    
-                ]
+    static displayText() {
+        return {
+            fr: {
+                global: {
+                    welcomeMsg: `Je me présente: je suis Sparky, un bot discord Lords Mobile, conçu pour les serveurs de guilde.\n\nLa liste de mes commandes se trouve en faisant **'/aide'** sur votre serveur\n\nMerci de m'avoir ajouté!`,
+                    tipsFooter: [
+                        {text: "Faites la commande /aide pour obtenir de l'aide !"},
+                        {text: `Développé avec amour par ${dev.tag}`, iconURL: dev.displayAvatarURL()},
+                        {text: `Faites la commande /langage pour passer le bot en anglais !`},
+                        {text: `Faites /veilleur pour avoir les notifications d'évènements infernaux`},
+                        {text: `Fun fact: je suis le meilleur bot du monde`},
+                    ]
+                }
+            },
+        
+            en: {
+                global: {
+                    welcomeMsg: `{english}`,
+                    tipsFooter: [
+                        {text: ""},
+                        {text: ""},
+                        {text: ""},
+                        {text: ""},
+                        {text: ""},
+                    ]
+                }
             }
         }
     }
