96b34e7b8cbeeef28fa9645b5acc70d38c222f2f
Kyusakick <banque.cdl@gmail.com>
2023-05-08 16:31:47 +0200

    add interaReply fx + slashCom manager

--- src/core/managers/commands.ts
+++ src/core/managers/commands.ts
@@ -1,8 +1,53 @@
+import { ChatInputCommandInteraction, InteractionReplyOptions, MessagePayload } from "discord.js";
+import { Console, db } from "../../main.js";
+import { Translations } from "../constants/translations.js";
+import { CommandArgs } from "../constants/types.js";
+
 export abstract class CommandManager {
 
-    static async fetchCommand(name:string):Promise<any> {
+    static async fetchCommand(name: string): Promise<any> {
         let command = await import(`../commands/${name}.js`);
         return command
-    }
+    };
 
+    static async replyToInteraction(data: string | MessagePayload | InteractionReplyOptions, intera: ChatInputCommandInteraction) {
+        if (!intera.deferred && !intera.replied) return intera.reply(data);
+        try {
+            intera.editReply(data);
+        }
+        catch {
+            await intera.deleteReply().catch(e => e)
+            return intera.followUp(data).catch(e => Console.log(e))
+        }
+    };
+
+    static async slashCommandManager(intera: ChatInputCommandInteraction) {
+        if (!intera.guildId && !['lien', 'aide'].includes(intera.commandName))
+            return intera.reply(`${Translations.displayText().fr.global.noCommandOffServer}\n\n${Translations.displayText().en.global.noCommandOffServer}`);
+
+        if (intera.guild && !await db.checkIfServerIsPresent(intera.guild))
+            await db.createServer(intera.guild.id, intera.guild.name);
+
+        let translation = await Translations.getServerTranslation(intera.guildId);
+
+        let command = await this.fetchCommand(intera.commandName);
+        if (!command) {
+            intera.reply({ content: translation.text.global.CommandExecutionError, ephemeral: true });
+            throw `Impossible de récupérer la commande ${intera.commandName}`;
+        }
+
+        let args:CommandArgs = { 
+            intera, 
+            translation 
+        };
+
+        try {
+            command.run(args);
+        }
+        catch (err) {
+            intera.deleteReply().catch(e => e);
+            this.replyToInteraction(translation.text.global.CommandExecutionError, intera);
+            Console.error(err);
+        }
+    };
 }
