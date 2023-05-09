aef45f6d74ac5fbfd8c1243d94f964f2a304cdd4
Kyusakick <banque.cdl@gmail.com>
2023-05-09 17:37:10 +0200

    format contact com structure

--- src/core/commands/contact.ts
+++ src/core/commands/contact.ts
@@ -1,16 +1,14 @@
 import { CommandInterface } from "../constants/types.js";
 import { CommandManager } from "../managers/commands.js";
-import { Console } from "../../main.js";
 
-async function run() {
-    Console.log("Ca marche yolo")
-}
 
-export const command:CommandInterface = {
+export const contact:CommandInterface = {
 
     permissionLevel: 1,
 
     commandStructure: CommandManager.baseSlashCommandBuilder("contact", "member"),
     
-    run
+    async run({ intera, translation }):Promise<void> {
+        
+    },
 }
