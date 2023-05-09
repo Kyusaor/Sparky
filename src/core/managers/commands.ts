68849e92a795b908e81b5c8fde4eea9d3c849a65
Kyusakick <banque.cdl@gmail.com>
2023-05-09 16:45:44 +0200

    adapt previous edit of displayText param

--- src/core/managers/commands.ts
+++ src/core/managers/commands.ts
@@ -1,15 +1,16 @@
 import { ChatInputCommandInteraction, InteractionReplyOptions, MessagePayload, SlashCommandBuilder } from "discord.js";
 import { Console, db } from "../../main.js";
 import { Translations } from "../constants/translations.js";
-import { CommandArgs } from "../constants/types.js";
+import {  CommandArgs, CommandInterface } from "../constants/types.js";
+import { readdirSync } from "fs";
 
 export abstract class CommandManager {
 
     static baseSlashCommandBuilder(name: string, perm: "member" | "admin" | "dev"): SlashCommandBuilder {
-        let defaultText = Translations.displayText();
+        let defaultText = [Translations.displayText("fr").commands, Translations.displayText("en").commands];
         let text = {
-            fr: defaultText.fr.commands[name as keyof typeof defaultText.fr.commands],
-            en: defaultText.en.commands[name as keyof typeof defaultText.en.commands],
+            fr: defaultText[0][name as keyof typeof defaultText[0]],
+            en: defaultText[1][name as keyof typeof defaultText[1]],
         };
 
         let build = new SlashCommandBuilder()
@@ -50,7 +51,7 @@ export abstract class CommandManager {
 
     static async slashCommandManager(intera: ChatInputCommandInteraction) {
         if (!intera.guildId && !['lien', 'aide'].includes(intera.commandName))
-            return intera.reply(`${Translations.displayText().fr.global.noCommandOffServer}\n\n${Translations.displayText().en.global.noCommandOffServer}`);
+            return intera.reply(`${Translations.displayText("fr").global.noCommandOffServer}\n\n${Translations.displayText("en").global.noCommandOffServer}`);
 
         if (intera.guild && !await db.checkIfServerIsPresent(intera.guild))
             await db.createServer(intera.guild.id, intera.guild.name);
@@ -60,7 +61,7 @@ export abstract class CommandManager {
         let command = await this.fetchCommand(intera.commandName);
         if (!command) {
             intera.reply({ content: translation.text.global.CommandExecutionError, ephemeral: true });
-            throw `Impossible de récupérer la commande ${intera.commandName}`;
+            Console.info(`Impossible de récupérer la commande ${intera.commandName}`);
         }
 
         let args: CommandArgs = {
@@ -73,7 +74,7 @@ export abstract class CommandManager {
         }
         catch (err) {
             intera.deleteReply().catch(e => e);
-            BaseCommand.prototype.reply(translation.text.global.CommandExecutionError, intera);
+            Command.prototype.reply(translation.text.global.CommandExecutionError, intera);
             Console.error(err);
         }
     };
