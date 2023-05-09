a4e586c62066f843c51924cce0fa64e4120d067a
Kyusakick <banque.cdl@gmail.com>
2023-05-09 17:38:06 +0200

    adapt tO to deploy

--- src/core/constants/translations.ts
+++ src/core/constants/translations.ts
@@ -17,7 +17,7 @@ export class Translations {
                     welcomeMsg: `Je me présente: je suis Sparky, un bot discord Lords Mobile, conçu pour les serveurs de guilde.\n\nLa liste de mes commandes se trouve en faisant **\`\`/aide\`\`** sur votre serveur\n\nMerci de m'avoir ajouté!`,
                     tipsFooter: [
                         {text: "Faites la commande /aide pour obtenir de l'aide !"},
-                        {text: `Développé avec amour par ${dev.tag}`, iconURL: dev.displayAvatarURL()},
+                        {text: `Développé avec amour par ${dev?.tag || `<@${DiscordValues.DEV_DISCORD_ID}>`}`, iconURL: dev?.displayAvatarURL()},
                         {text: `Faites la commande /langage pour passer le bot en anglais !`},
                         {text: `Faites /veilleur pour avoir les notifications d'évènements infernaux`},
                         {text: `Fun fact: je suis le meilleur bot du monde`},
