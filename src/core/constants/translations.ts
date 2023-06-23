d1155d8cf1f48ccf6b3cb85e1f9b7e2ea6306a19
Kyusakick <banque.cdl@gmail.com>
2023-06-23 18:27:58 +0200

    refix tO organisation

--- src/core/constants/translations.ts
+++ src/core/constants/translations.ts
@@ -11,7 +11,98 @@ export class Translations {
     }
 
     static displayText(language: textLanguage) {
-        return this.text[language];
+        let text = {
+            fr: {
+                global: {
+                    welcomeMsg: `Je me présente: je suis Sparky, un bot discord Lords Mobile, conçu pour les serveurs de guilde.\n\nLa liste de mes commandes se trouve en faisant **\`\`/aide\`\`** sur votre serveur\n\nMerci de m'avoir ajouté!`,
+                    tipsFooter: [
+                        {text: "Faites la commande /aide pour obtenir de l'aide !"},
+                        {text: `Développé avec amour par ${dev?.username || `<@${DiscordValues.DEV_DISCORD_ID}>`}`, iconURL: dev?.displayAvatarURL()},
+                        {text: `Faites la commande /langage pour passer le bot en anglais !`},
+                        {text: `Faites /veilleur pour avoir les notifications d'évènements infernaux`},
+                        {text: `Fun fact: je suis le meilleur bot du monde`},
+                    ],
+                    noLinkInDm: ":flag_fr: Bonjour, pour m'ajouter à votre serveur utilisez plutôt ce lien!",
+                    noCommandOffServer: ":flag_fr: Les commandes sont à réaliser sur un serveur !",
+                    defaultCommandReply: "Impossible d'exécuter la commande :confused:",
+                    CommandExecutionError: "Une erreur est survenue pendant l\'éxecution de la commande!",
+                },
+                permissions: {
+                    MissingPermissions: "Oups, il semblerait qu'il me manque les permissions suivantes:\n",
+                    flags: {
+                        AddReactions: "Ajouter des réactions",
+                        AttachFiles: "Joindre des fichiers",
+                        EmbedLinks: "Intégrer des liens",
+                        ManageRoles: "Gérer les rôles",
+                        ManageChannels: "Gérer les salons",
+                        UseExternalEmojis: "Utiliser des emojis externes",
+                    }
+                },
+                helpMention: {
+                    title: "Perdu?",
+                    description: "Pour obtenir la liste de mes commandes, faites **/aide**",
+                },
+                commands: {
+                    contact: {
+                        name: "contact",
+                        description: "Nous contacter (discord ou mail)",
+                        text: {
+                            content: [
+                                "Besoin d'aide sur le bot, de signaler un bug ou simplement discuter avec des joueurs de Lords Mobile? Voilà le lien du serveur :wink:\n",
+                                "\n\nPour contacter le développeur, vous pouvez également envoyer un mail à __**"
+                            ],
+                        },
+                    },
+                },
+            },
+        
+            en: {
+                global: {
+                    welcomeMsg: `{english}`,
+                    tipsFooter: [
+                        {text: "{english}"},
+                        {text: "{english}"},
+                        {text: "{english}"},
+                        {text: "{english}"},
+                        {text: "{english}"},
+                    ],
+                    noLinkInDm: "{english}",
+                    noCommandOffServer: "{english}",
+                    CommandExecutionError: "{english}",
+                    defaultCommandReply: "{english}"
+                },
+                permissions: {
+                    MissingPermissions: "{english}",
+                    flags: {
+                        AddReactions: "{english}",
+                        AttachFiles: "{english}",
+                        EmbedLinks: "{english}",
+                        ManageRoles: "{english}",
+                        ManageChannels: "{english}",
+                        UseExternalEmojis: "{english}",
+                    }
+                },
+                helpMention: {
+                    title: "{english}",
+                    description: "{english}",
+                },
+                commands: {
+                    contact: {
+                        name: "contact",
+                        description: "{english}",
+                        text: {
+                            content: [
+                                "{english}",
+                                "{english}"
+                            ]
+                        },
+                    },
+                },
+    
+            }
+        };
+
+        return text[language];
     }
 
     static displayFullText() {
@@ -55,95 +146,4 @@ export class Translations {
         titan: { fr: `Titan des Marées`, en: `Tidal Titan` },
         wyrm: { fr: `Wyrm de Jade`, en: `Jade Wyrm` },
     }
-
-    static text = {
-        fr: {
-            global: {
-                welcomeMsg: `Je me présente: je suis Sparky, un bot discord Lords Mobile, conçu pour les serveurs de guilde.\n\nLa liste de mes commandes se trouve en faisant **\`\`/aide\`\`** sur votre serveur\n\nMerci de m'avoir ajouté!`,
-                tipsFooter: [
-                    {text: "Faites la commande /aide pour obtenir de l'aide !"},
-                    {text: `Développé avec amour par ${dev?.tag || `<@${DiscordValues.DEV_DISCORD_ID}>`}`, iconURL: dev?.displayAvatarURL()},
-                    {text: `Faites la commande /langage pour passer le bot en anglais !`},
-                    {text: `Faites /veilleur pour avoir les notifications d'évènements infernaux`},
-                    {text: `Fun fact: je suis le meilleur bot du monde`},
-                ],
-                noLinkInDm: ":flag_fr: Bonjour, pour m'ajouter à votre serveur utilisez plutôt ce lien!",
-                noCommandOffServer: ":flag_fr: Les commandes sont à réaliser sur un serveur !",
-                defaultCommandReply: "Impossible d'exécuter la commande :confused:",
-                CommandExecutionError: "Une erreur est survenue pendant l\'éxecution de la commande!",
-            },
-            permissions: {
-                MissingPermissions: "Oups, il semblerait qu'il me manque les permissions suivantes:\n",
-                flags: {
-                    AddReactions: "Ajouter des réactions",
-                    AttachFiles: "Joindre des fichiers",
-                    EmbedLinks: "Intégrer des liens",
-                    ManageRoles: "Gérer les rôles",
-                    ManageChannels: "Gérer les salons",
-                    UseExternalEmojis: "Utiliser des emojis externes",
-                }
-            },
-            helpMention: {
-                title: "Perdu?",
-                description: "Pour obtenir la liste de mes commandes, faites **/aide**",
-            },
-            commands: {
-                contact: {
-                    name: "contact",
-                    description: "Nous contacter (discord ou mail)",
-                    text: {
-                        content: [
-                            "Besoin d'aide sur le bot, de signaler un bug ou simplement discuter avec des joueurs de Lords Mobile? Voilà le lien du serveur :wink:\n",
-                            "\n\nPour contacter le développeur, vous pouvez également envoyer un mail à __**"
-                        ],
-                    },
-                },
-            },
-        },
-    
-        en: {
-            global: {
-                welcomeMsg: `{english}`,
-                tipsFooter: [
-                    {text: "{english}"},
-                    {text: "{english}"},
-                    {text: "{english}"},
-                    {text: "{english}"},
-                    {text: "{english}"},
-                ],
-                noLinkInDm: "{english}",
-                noCommandOffServer: "{english}",
-                CommandExecutionError: "{english}",
-                defaultCommandReply: "{english}"
-            },
-            permissions: {
-                MissingPermissions: "{english}",
-                flags: {
-                    AddReactions: "{english}",
-                    AttachFiles: "{english}",
-                    EmbedLinks: "{english}",
-                    ManageRoles: "{english}",
-                    ManageChannels: "{english}",
-                    UseExternalEmojis: "{english}",
-                }
-            },
-            helpMention: {
-                title: "{english}",
-                description: "{english}",
-            },
-            commands: {
-                contact: {
-                    name: "contact",
-                    description: "{english}",
-                    text: {
-                        content: [
-                            "{english}",
-                            "{english}"
-                        ]
-                    },
-                },
-            },
-
-        }
-    };
 }
