135bb3e0223255443f2321294365e55e7f7eff2c
Kyusakick <banque.cdl@gmail.com>
2023-06-24 15:29:00 +0200

    add choices to compo cmd builder

--- src/core/commands/compo.ts
+++ src/core/commands/compo.ts
@@ -1,3 +1,5 @@
+import { APIApplicationCommandOptionChoice } from "discord.js";
+import { Translations } from "../constants/translations.js";
 import { CommandInterface } from "../constants/types.js";
 import { CommandManager } from "../managers/commands.js";
 
@@ -5,10 +7,25 @@ export const compo:CommandInterface = {
 
     permissionLevel: 1,
 
-    commandStructure: CommandManager.baseSlashCommandBuilder("compo", "member"),
+    commandStructure: CommandManager.baseSlashCommandBuilder("compo", "member")
+        .addStringOption(monster =>
+            monster.setName(`Monster`)
+                .setDescription('The monster you want the lineup for')
+                .setRequired(true)
+                .addChoices(...getMobsChoicesList())
+        ),
 
     async run({ intera, translation }): Promise<void> {
         let imagePath = `./ressources/images/mob/${translation.language}/${intera.options.getString('mob')}.png`;
         
     }
+}
+
+function getMobsChoicesList():APIApplicationCommandOptionChoice<string>[] {
+    let list = []
+    let translations = Translations.mobs;
+    for(let mob of Object.keys(translations)) {
+        list.push({ name: translations[mob as keyof typeof translations].en, name_localizations: { fr: translations[mob as keyof typeof translations].fr}, value: mob})
+    }
+    return list;
 }
