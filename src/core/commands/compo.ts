f5e4703c701b9741bcd2b082b14b7148b2a4ea37
Kyusakick <banque.cdl@gmail.com>
2023-08-06 17:29:25 +0200

    use getChoices fx for compo deploy

--- src/core/commands/compo.ts
+++ src/core/commands/compo.ts
@@ -1,7 +1,7 @@
-import { APIApplicationCommandOptionChoice, SlashCommandStringOption } from "discord.js";
-import { CommandInterface, TranslationCacheType, TranslationObject } from "../constants/types.js";
+import { SlashCommandStringOption } from "discord.js";
+import { CommandInterface } from "../constants/types.js";
 import { Command, CommandManager } from "../managers/commands.js";
-import { existsSync, readFileSync } from "fs";
+import { existsSync } from "fs";
 
 export const compo:CommandInterface = {
 
@@ -11,7 +11,7 @@ export const compo:CommandInterface = {
         .addStringOption(
             (Command.generateCommandOptionBuilder("compo", "monster", "string") as SlashCommandStringOption)
                 .setRequired(true)
-                .addChoices(...getMobsChoicesList())
+                .addChoices(...Command.getChoices("compo", "mobs"))
         ),
 
     async run({ intera, language }): Promise<void> {
@@ -20,16 +20,4 @@ export const compo:CommandInterface = {
             throw `Fichier ./ressources/images/mob/${language}/${intera.options.getString('monster')}.png introuvable`
         await Command.prototype.reply({ files: [ imagePath] }, intera);
     }
-}
-
-function getMobsChoicesList():APIApplicationCommandOptionChoice<string>[] {
-    const frJSON:TranslationObject = JSON.parse(readFileSync(`./ressources/text/fr.json`, 'utf-8'));
-    const enJSON:TranslationObject = JSON.parse(readFileSync(`./ressources/text/en.json`, 'utf-8'));
-    let translations:TranslationCacheType = { fr: frJSON, en: enJSON };
-
-    let list = []
-    for(let mob of Object.keys(translations.fr.mobs)) {
-        list.push({ name: translations.en.mobs[mob as keyof typeof translations.fr.mobs], name_localizations: { fr: translations.fr.mobs[mob as keyof typeof translations.fr.mobs]}, value: mob})
-    }
-    return list;
 }
