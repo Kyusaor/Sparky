90ed3ffdf39dfead91dbbdc5fd9dc24772e3eb88
Kyusakick <banque.cdl@gmail.com>
2023-08-27 15:38:25 +0200

    add data cmd

---
+++ src/core/commands/data.ts
@@ -0,0 +1,28 @@
+import { existsSync } from "fs";
+import { db } from "../../main.js";
+import { CommandInterface } from "../constants/types.js";
+import { Command, CommandManager } from "../managers/commands.js";
+import { Utils } from "../utils.js";
+
+export const data:CommandInterface = {
+
+    permissionLevel: 3,
+
+    commandStructure: CommandManager.baseSlashCommandBuilder("data", "dev"),
+
+    async run({ intera, commandText }) {
+        await db.generateBackup();
+
+        let day = Utils.format2DigitsNumber(new Date().getDate());
+        let month = Utils.format2DigitsNumber(new Date().getMonth() + 1);
+
+        let path = `./data/backups/${month}/backup-${day}-${month}.sql`
+
+        console.log(path)
+        if(!existsSync(path))
+            return Command.prototype.reply(commandText.error, intera);
+
+        await intera.user.send({files: [path]});
+        Command.prototype.reply(commandText.success, intera);
+    },
+}
