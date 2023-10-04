98318b4d14f9aa4228d7972c771707a99c31ec39
Kyusakick <banque.cdl@gmail.com>
2023-10-04 17:56:06 +0200

    add rss cmd file

---
+++ src/core/commands/ressources.ts
@@ -0,0 +1,36 @@
+import { InteractionReplyOptions, SlashCommandStringOption } from "discord.js";
+import { CommandInterface } from "../constants/types";
+import { Command, CommandManager } from "../managers/commands";
+import { existsSync } from "fs";
+
+export const ressources:CommandInterface = {
+    
+    permissionLevel: 1,
+
+    commandStructure: CommandManager.baseSlashCommandBuilder("ressources", "member")
+        .addStringOption(
+            (Command.generateCommandOptionBuilder("ressources", "target", "string") as SlashCommandStringOption)
+                .addChoices(...Command.getChoices("ressources", "target"))
+        ),
+
+    cacheLockScope: "none",
+
+    run({ intera, commandText }) {
+
+        let option = intera.options.getString('target');
+
+        let path = `../../../ressources/images/rss-cmd/${option}.png`
+        let text = commandText[option as keyof typeof commandText];
+
+        let payload:InteractionReplyOptions = {
+        };
+
+        if(existsSync(path))
+            payload.files = [path];
+
+        if(text)
+            payload.content = text;
+
+        Command.prototype.reply(payload, intera);
+    },
+}
